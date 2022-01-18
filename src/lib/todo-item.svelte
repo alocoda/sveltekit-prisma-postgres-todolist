fc50bf6d1cec4b201f61ef0200bbdb6052768c39
Aloysius <aloysiusddev5@pm.me>
2022-01-18 19:23:59 +0000

    Add global CSS. Add app CSS.

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,15 +1,21 @@
-<div>
+<style>
+    /* .todo {
+
+    } */
+</style>
+
+<div class="todo">
     <form action="" method="">
         <input type="hidden" name="done" value="">
-        <button aria-label="Mark done/ not done">Done/ NotDone</button>
+        <button aria-label="Mark done/ not done" class="toggle">Done/ NotDone</button>
     </form>
 
-    <form action="" method="">
+    <form action="" method="" class="text">
         <input type="text">
-        <button aria-label="Save todo">Save</button>
+        <button aria-label="Save todo" class="save">Save</button>
     </form>
 
     <form action="" method="">
-        <button aria-label="Delete todo">Delete</button>
+        <button aria-label="Delete todo" class="delete">Delete</button>
     </form>
 </div>
