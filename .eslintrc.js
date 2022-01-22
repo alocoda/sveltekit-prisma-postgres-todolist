148ac54124b84bd50de8d6447478c0bb6825a15c
Aloysius <aloysiusddev5@pm.me>
2022-01-22 15:57:17 +0000

    Enhance HTML forms.

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "extends": [
+    "airbnb",
+    "plugin:prettier/recommended",
+    "prettier/react"
+  ],
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "jest": true,
+    "node": true
+  },
+  "rules": {
+    "jsx-a11y/href-no-hash": ["off"],
+    "react/jsx-filename-extension": ["warn", { "extensions": [".js", ".jsx"] }],
+    "max-len": [
+      "warn",
+      {
+        "code": 80,
+        "tabWidth": 2,
+        "comments": 80,
+        "ignoreComments": false,
+        "ignoreTrailingComments": true,
+        "ignoreUrls": true,
+        "ignoreStrings": true,
+        "ignoreTemplateLiterals": true,
+        "ignoreRegExpLiterals": true
+      }
+    ]
+  }
+}
+
+// https://gist.github.com/bradtraversy/aab26d1e8983d9f8d79be1a9ca894ab4
